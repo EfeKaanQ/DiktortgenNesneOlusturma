f8562b339baccdb02fede1e7ca3f27f2cb014b05
efekaan <efekaan@EKD>
2023-10-31 11:56:03 +0300

    Proje dosyası ekle.

---
+++ DiktortgenNesneOlusturma/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiktortgenNesneOlusturma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(13, 40);
+            Console.WriteLine("Diktörtgeenin Alanı = " + dikdortgen.AlanHesapla());
+            Console.WriteLine("Diktörtgeenin Çevresi = " + dikdortgen.CevreHesapla());
+
+        }
+    }
+    class Dikdortgen
+    {
+        private uint kisaKenar,uzunKenar;
+        public Dikdortgen(uint kisaKenar, uint uzunKenar)
+        {
+            this.kisaKenar = kisaKenar;
+            this.uzunKenar = uzunKenar;
+        }
+        public uint AlanHesapla()
+        {
+            return kisaKenar * uzunKenar;
+        }
+        public uint CevreHesapla()
+        {
+            return 2 * (kisaKenar + uzunKenar);
+        }
+    }
+
+}
---
+++ DiktortgenNesneOlusturma/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("DiktortgenNesneOlusturma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiktortgenNesneOlusturma")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("d10a2040-f4af-4606-b988-02fa11f12923")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
